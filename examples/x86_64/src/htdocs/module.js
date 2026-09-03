a0a032015f73218b880f2a879acbaf38bc88d41e
themainfascist-a11y <themainfascist@gmail.com>
2026-09-03 02:16:12 -0700

    feat: enable graphical SDL x86_64 guest

--- examples/x86_64/src/htdocs/module.js
+++ examples/x86_64/src/htdocs/module.js
@@ -1,7 +1,9 @@
 Module['arguments'] = [
-    '-nographic', '-m', '512M', '-accel', 'tcg,tb-size=500',
-    //Use the following to enable MTTCG
-    //'-nographic', '-m', '512M', '-accel', 'tcg,tb-size=500,thread=multi', '-smp', '4,sockets=4',
+    '-m', '512M',
+    '-accel', 'tcg,tb-size=500',
+    // Use the following to enable MTTCG.
+    // '-m', '512M', '-accel', 'tcg,tb-size=500,thread=multi', '-smp', '4,sockets=4',
+    '-display', 'sdl',
     '-L', '/pack/',
     '-nic', 'none',
     '-drive', 'if=virtio,format=raw,file=/pack/rootfs.bin',
